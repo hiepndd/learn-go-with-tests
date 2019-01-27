db73f4038a85e2d8c23dd0393f9fc229bd048220
hiepndd <nguyendangdaihiep@gmail.com>
2019-01-27 19:08:04 +0700

    structs

---
+++ structs/shapes.go
@@ -0,0 +1,26 @@
+package structs
+
+import (
+	"math"
+)
+
+type Circle struct {
+	Radius float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
