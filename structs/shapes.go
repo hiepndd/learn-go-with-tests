8a18295d891df8e3fe4d5a79f235d224c9fb1ec7
hiepndd <nguyendangdaihiep@gmail.com>
2019-02-18 18:31:07 +0700

    refactor readable test-case

--- structs/shapes.go
+++ structs/shapes.go
@@ -13,6 +13,11 @@ type Rectangle struct {
 	Height float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -23,4 +28,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
